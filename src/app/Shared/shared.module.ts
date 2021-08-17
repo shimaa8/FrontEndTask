b711e749442b9700f9a41d2fe81b5393e71ad492
sh <shimaasaeed173@yahoo.com>
2021-08-17 13:59:11 +0200

    project arch

---
+++ src/app/Shared/shared.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedRoutingModule } from './shared-routing.module';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { TeamComponent } from './team/team.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    AboutComponent,
+    PortfolioComponent,
+    TestimonialsComponent,
+    TeamComponent,
+    ClientsComponent,
+    ContactUsComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedRoutingModule
+  ]
+})
+export class SharedModule { }
